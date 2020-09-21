4a69fc9a4b01cffd8428a683b7fa2fe8bf8b5436
infernezor@gmail.com <infernezor@gmail.com>
2020-09-21 17:50:35 -0400

    clean up for final implimentation.

--- ui/src/util/api-conn.js
+++ ui/src/util/api-conn.js
@@ -41,14 +41,8 @@ export const getList = async (gameName) => {
 };
 
 export const submitAchievement = async (game, achievement, user, token) => {
-  console.log('reporting from submitAchievement: ', achievement, user);
-  console.log('achievement ', achievement);
   const url = `http://localhost:5000/user/achievement`;
   const packet = { Game: game, Achievement: achievement, User: user };
-  //
-  // const { getAccessTokenSilently } = useAuth0();
-  // const token = await getAccessTokenSilently();
-  //
 
   console.log('the recieved user is: ', user);
 
@@ -63,7 +57,6 @@ export const submitAchievement = async (game, achievement, user, token) => {
       body: JSON.stringify(packet),
     });
     const responseData = await response.json();
-    console.log('the received data from the backend is: ', responseData);
     return responseData;
   } catch (e) {
     console.log('catch block');
@@ -72,24 +65,5 @@ export const submitAchievement = async (game, achievement, user, token) => {
 };
 
 //test
-
-export const post = async (url, data) => {
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    //this is missing the consuming of the fetch!
-    return response;
-  } catch (e) {
-    console.log(e);
-    console.log('qua?');
-    return e;
-  }
-};
 
 export const API_URL = 'http://localhost:5000';

279b97860ca5c4db578ae54365551b3cc44594cb
infernezor@gmail.com <infernezor@gmail.com>
2020-10-27 15:37:24 -0400

    turning function back on to see if it works within a lambda setup.

--- lambda/routes/search.js
+++ lambda/routes/search.js
@@ -2,17 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-// const DataBase = require('../models/functions');
+const DataBase = require('../models/functions');
 
 /* GET search page. */
 router.get('/', async (req, res) => {
   res.send('begin the search!');
   try {
-    // const response = await DataBase.getGameListJson(req.headers.game);
+    const response = await DataBase.getGameListJson(req.headers.game);
     console.log('hello from the search function, lambda!');
 
     res.send('sending from search.');
-    // res.json(response).status(200);
+    res.json(response).status(200);
   } catch (e) {
     console.log('the api on backend failed to fetch in search.js.');
     return e;

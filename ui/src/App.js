10cae6fd5d7cd1fd176bd94680632ade5b19542e
infernezor@gmail.com <infernezor@gmail.com>
2020-10-31 03:51:11 -0400

    routing issues with the new domain name. Perhaps an exact routing will help.

--- ui/src/App.js
+++ ui/src/App.js
@@ -33,9 +33,9 @@ function App() {
 
       <Router>
         <Route path='/' component={Landing} exact />
-        <Route path='/game/:id' component={GameStub} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/about' component={About} />
+        <Route exact path='/game/:id' component={GameStub} />
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/about' component={About} />
       </Router>
     </>
   );

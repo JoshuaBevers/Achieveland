97a7b7ad57f75eca061a5fdcdeb22f3674cd4614
infernezor@gmail.com <infernezor@gmail.com>
2020-10-30 20:54:41 -0400

    build issue on aws.

--- ui/src/components/claim-achievement/claim-button.jsx
+++ ui/src/components/claim-achievement/claim-button.jsx
@@ -36,7 +36,6 @@ const ClaimAchievementButton = (props) => {
     if (UserAchievements !== null) {
       console.log('user achievements is not null: ', UserAchievements);
       const number = UserAchievements.find((x) => {
-        console.log(x);
         x.achievement_no === props.achievement.id;
       });
       console.log('the number is: ', number);

5e6c5d23561260095d4e49b37c1b37aff3272909
infernezor@gmail.com <infernezor@gmail.com>
2020-11-02 19:39:11 -0500

    further message test.

--- ui/src/components/game-stub.jsx
+++ ui/src/components/game-stub.jsx
@@ -81,6 +81,7 @@ function GameStub() {
         const Token = await getAccessTokenSilently({
           scope: 'read:current_user',
         });
+        console.log('Token is: ', Token, 'And game id is: ', game.id);
 
         if (game.id !== undefined) {
           const userData = await getUserAchievements(
